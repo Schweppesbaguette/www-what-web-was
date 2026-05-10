bdaf09d0a5ac018c7b494b664b8be6de27e4715f
Schweppesbaguette <mintmintpeppermint@gmail.com>
2026-05-09 20:37:47 -0400

    Update preload.js

--- preload.js
+++ preload.js
@@ -2,11 +2,11 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
   signonComplete: () => ipcRenderer.send('signon-complete'),
   setupComplete: () => ipcRenderer.send('setup-complete'),
+  signOff: () => ipcRenderer.send('sign-off'),
   openRadio: () => ipcRenderer.send('open-radio'),
   openTV: () => ipcRenderer.send('open-tv'),
   closeRadio: () => ipcRenderer.send('close-radio'),
   closeTV: () => ipcRenderer.send('close-tv'),
   minimizeRadio: () => ipcRenderer.send('minimize-radio'),
   minimizeTV: () => ipcRenderer.send('minimize-tv'),
-  onMessage: (cb) => ipcRenderer.on('message', (e, msg) => cb(msg)),
 });
